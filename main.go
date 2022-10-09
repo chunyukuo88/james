3a9f348e634b556be6bf2dce90bfe53b967aa023
Alex Gochenour <alex.gochenour@offorhealth.com>
2022-10-08 23:26:33 -0400

    Can grab a URL param and print it in the URL.

--- main.go
+++ main.go
@@ -21,11 +21,22 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>This is the FAQ page. But no one has asked any questions frequently so there's nothing else here.</h2>")
 }
 
+func galleryHandler(w http.ResponseWriter, r *http.Request) {
+	galleryId := chi.URLParam(r, "galleryId")
+	fmt.Fprintf(w, "<h1>Welcome to the %v gallery!</h1>", galleryId)
+}
+
+func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Page not found!", http.StatusNotFound)
+}
+
 func main() {
 	router := chi.NewRouter()
 	router.Get("/", rootHandler)
 	router.Post("/contact", contactHandler)
 	router.Get("/faq", faqHandler)
+	router.Get("/galleries/{galleryId}", galleryHandler)
+	router.NotFound(pageNotFoundHandler)
 	fmt.Println("Starting my rad server on port 3000...")
 	http.ListenAndServe(":3000", router)
 }
